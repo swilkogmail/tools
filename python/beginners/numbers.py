9de5716e8ea0d24a6367e10a6f7c809c527854ff
StephenWilkinsTR <stephen.wilkins@thomsonreuters.com>
2022-12-22 09:02:48 +0000

    number and strings

---
+++ python/beginners/numbers.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+num = 1+1
+flt = 1.0 + 1.0
+big_num = 4.5e9
+
+
+print(big_num)
+print(num)
+print(flt)
+
+## operators
+
+num1 = 5
+num2 = 3
+
+print(num1 + num2)
+print(num1 / num2)
+print(num1 * num2)
+print(num2 / num1)
+print(num2 // num1) # floor division - how many times it evenly divides
+print(num2 % num1) # mod - returns the rem  any iteger using mod 2, i.e. num % 2 will return 0 if even 1 if odd
+print(num1 ** num2)
--- python/beginners/strings.py
+++ python/beginners/strings.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 # single quotes and double quotes are the same although
 # can be changed if you need to use a ' or a " inside the string
-print('string')
-print("string")
+print('pass')
+print("word")
 print('''This
 is a 
 multi line string''')
@@ -12,3 +12,22 @@ is a
 multi line string'''
 print(var)
 # var will be stored as a single lines with /n characters
+
+var = 'ha'
+var *= 4 # multiple var by 4 times and reassign to var
+print(var)
+
+'''
+Objects encapsulate two things
+    state: 
+        in string it's sequence of characters, it's what the object holds
+    behaviour:
+        method these can act on the state
+'''
+var = 'my_string' # my_string is the state
+print(var.upper()) # upper is the method
+
+# escape sequences
+print("Tab\tDelimited")
+print("New\nLine")
+print("New\\nLine") # unescaped sequence
