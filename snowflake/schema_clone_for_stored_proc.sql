e86cd98f249590bf91238e26d863778747bd87ff
Stephen Wilkins <swilko@gmail.com>
2023-09-06 09:05:59 +0100

    added some stuff

--- snowflake/schema_clone_for_stored_proc.sql
+++ snowflake/schema_clone_for_stored_proc.sql
@@ -1,8 +1,11 @@
--- Create the procedure
-CREATE OR REPLACE PROCEDURE clone_schema_with_privileges(
-  existing_schema_name VARCHAR,
-  new_schema_name VARCHAR,
-  other_user_name VARCHAR)
+/*
+In order to streamline our time consuming and convoluted, object level, cloning procedure we would like to have the capability to clone our prod schema at the Schema level.
+
+We would like to incorporate using the python connector in to our existing workfow with a simple procedure call.  One suggested implementation could be below:
+*/
+
+-- as admin user
+CREATE OR REPLACE PROCEDURE clone_schema_with_privileges(new_schema_name VARCHAR)
 RETURNS STRING
 LANGUAGE SQL
 EXECUTE AS OWNER
@@ -12,17 +15,19 @@ DECLARE
   clone_sql STRING;
   grant_sql STRING;
 BEGIN
-  -- Step 1: Clone the schema
-  clone_sql := 'CREATE SCHEMA ' || new_schema_name || ' CLONE ' || existing_schema_name;
+
+  clone_sql := 'CREATE SCHEMA ' || new_schema_name || ' CLONE MYDATASPACE.A205384_DEP_EAW';
   EXECUTE IMMEDIATE clone_sql;
-  
-  -- Step 2: Grant privileges to the other user
-  grant_sql := 'GRANT USAGE, CREATE, MODIFY ON SCHEMA ' || new_schema_name || ' TO ' || other_user_name;
+ 
+  grant_sql := 'GRANT USAGE, CREATE, MODIFY ON SCHEMA ' || new_schema_name || ' TO a205384_dep_eaw_preprod_mds_owner';
   EXECUTE IMMEDIATE grant_sql;
-  
-  RETURN 'Schema ' || new_schema_name || ' cloned successfully and privileges granted to ' || other_user_name || '.';
+ 
+  RETURN 'Clone Schema ' || new_schema_name || ' Created.';
 EXCEPTION
   WHEN OTHERS THEN
     RETURN 'Error: ' || SQLERRM;
 END;
 $$;
+
+-- grant to our role
+grant usage on procedure clone_schema_with_privileges to a205384_dep_eaw_preprod_mds_owner;
